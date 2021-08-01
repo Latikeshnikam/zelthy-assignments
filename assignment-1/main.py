cac993136243353be0fe459d97ecf08eec5956fe
Latikesh Nikam <latikesh2121@gmail.com>
2021-08-01 22:56:24 +0530

    Changes in main.py file

--- assignment-1/main.py
+++ assignment-1/main.py
@@ -7,17 +7,19 @@ import csv
 
 app = Flask(__name__, template_folder='template')
 
+# To display the fetched data
 @app.route('/', methods=['GET'])
 def get_data():
     data = requests.get('https://606f76d385c3f0001746e93d.mockapi.io/api/v1/auditlog').content
     return render_template('display_info.html',data = json.loads(data))
 
+#route to import json data to csv
 @app.route('/convertCSV')
 def response_to_csv():
     data = requests.get('https://606f76d385c3f0001746e93d.mockapi.io/api/v1/auditlog').content
     data = json.loads(data)
     f = csv.writer(open("output.csv", "w+"))
-    f.writerow(["ID", "Description", "Date", "Action", "User ID", "Criticality"])
+    f.writerow(["id", "description", "date", "action", "user id", "criticality"])
     for x in data:
         f.writerow([x["id"],
                     x["description"],
